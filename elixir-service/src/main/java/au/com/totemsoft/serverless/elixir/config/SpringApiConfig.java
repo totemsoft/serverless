cd5eaeb6965de73daf90c63014c60c870a795a83
shibaevv <shibaev.valera@gmail.com>
2020-12-19 22:09:04 +1100

    ClientApi (wip)

--- elixir-service/src/main/java/au/com/totemsoft/serverless/elixir/config/SpringApiConfig.java
+++ elixir-service/src/main/java/au/com/totemsoft/serverless/elixir/config/SpringApiConfig.java
@@ -1,24 +1,30 @@
 package au.com.totemsoft.serverless.elixir.config;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 import au.com.totemsoft.serverless.elixir.controller.ClientController;
 import au.com.totemsoft.serverless.elixir.controller.SurveyController;
@@ -37,17 +43,27 @@ import au.com.totemsoft.serverless.elixir.service.workdocs.AwsWorkDocsServiceImp
     AwsWorkDocsServiceImpl.class,
     AwsS3ServiceImpl.class,
 })
-public class SpringApiConfig {
+public class SpringApiConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper()));
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper()
+        return SpringLambdaContainerHandler.getObjectMapper()
             .setSerializationInclusion(Include.NON_NULL)
             .disable(MapperFeature.DEFAULT_VIEW_INCLUSION)
             .enable (DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .enable (DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
             //.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm a z"))
-            .registerModule(new AfterburnerModule());
+            ;
+    }
+
+    @Bean
+    public HttpMessageConverter<?> jsonMessageConverter(@Autowired ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
     }
 
     /*
@@ -67,11 +83,11 @@ public class SpringApiConfig {
     }
 
     /*
-     * optimization - avoids creating default exception resolvers; not required as the serverless container handles
-     * all exceptions
+     * optimization - avoids creating default exception resolvers;
+     * not required as the serverless container handles all exceptions
      *
-     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
-     * an expensive ObjectMapper instance.
+     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object,
+     * including an expensive ObjectMapper instance.
      * 
      * To enable custom @ControllerAdvice classes remove this bean.
      */
